b759960c77c59f596b6cc4934ba766a4549c5e1b
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2024-02-29 18:02:13 +0700

    upload code

--- js/modules/selectDropdownModule.js
+++ js/modules/selectDropdownModule.js
@@ -1,55 +1,25 @@
 export default function selectDropdownModule() {
-  $(".custom-select").each(function () {
-    var classes = $(this).attr("class"),
-      id = $(this).attr("id"),
-      name = $(this).attr("name");
-    var template = '<div class="' + classes + '">';
-    template +=
-      '<span class="custom-select-trigger">' +
-      $(this).attr("placeholder") +
-      "</span>";
-    template += '<div class="custom-options">';
-    $(this)
-      .find("option")
-      .each(function () {
-        template +=
-          '<span class="custom-option ' +
-          $(this).attr("class") +
-          '" data-value="' +
-          $(this).attr("value") +
-          '">' +
-          $(this).html() +
-          "</span>";
-      });
-    template += "</div></div>";
+  // Change option selected
+  const label = document.querySelector(".dropdown__filter-selected");
+  const options = document.querySelectorAll(".dropdown__select-option");
 
-    $(this).wrap('<div class="custom-select-wrapper"></div>');
-    $(this).hide();
-    $(this).after(template);
-  });
-
-  $(".custom-select-trigger").on("click", function (event) {
-    $("html").one("click", function () {
-      $(".custom-select").removeClass("opened");
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      label.textContent = option.textContent;
     });
-    $(this).parents(".custom-select").toggleClass("opened");
-    event.stopPropagation();
   });
 
-  $(".custom-option").on("click", function () {
-    var value = $(this).data("value");
-    var $select = $(this).parents(".custom-select-wrapper").find("select");
-    $select.val(value);
-    $select.trigger("change"); // Trigger the change event on the select element
-    $(this)
-      .parents(".custom-options")
-      .find(".custom-option")
-      .removeClass("selection");
-    $(this).addClass("selection");
-    $(this).parents(".custom-select").removeClass("opened");
-    $(this)
-      .parents(".custom-select")
-      .find(".custom-select-trigger")
-      .text($(this).text());
+  // Close dropdown onclick outside
+  document.addEventListener("click", (e) => {
+    const toggle = document.querySelector(".dropdown__switch");
+    const element = e.target;
+
+    if (element === toggle) return;
+
+    const isDropdownChild = element.closest(".dropdown__filter");
+
+    if (!isDropdownChild) {
+      toggle.checked = false;
+    }
   });
 }
