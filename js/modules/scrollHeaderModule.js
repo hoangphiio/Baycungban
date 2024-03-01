6dc3be3fe65484506de8759088125e2e31e82956
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2024-03-01 16:01:39 +0700

    upload code

--- js/modules/scrollHeaderModule.js
+++ js/modules/scrollHeaderModule.js
@@ -1,7 +1,7 @@
 export default function scrollHeaderModule() {
   // Scroll header add class
   $(window).on("scroll", function () {
-    if ($(window).scrollTop() > 40) {
+    if ($(window).scrollTop() > 20) {
       $(".header").addClass("in");
     } else {
       //remove the background property so it comes transparent again (defined in your css)
